cb211dba8a33b1744ced59dbac39f33dee100046
carmel13-meet <16carmel@gmail.com>
2013-11-26 21:57:45 +0200

    added something

--- Lab2/database.py
+++ Lab2/database.py
@@ -1,8 +1,8 @@
 dictionary = {}
 def dictionary():
 	for i in range(0, 3):
-		name = raw_input("What's your name bitch ?")
-		age = raw_input("What's your age motherfucker ?")
+		name = raw_input("What's your name ?")
+		age = raw_input("What's your age ?")
 		dictionary.update({"Name #" + str(i): name, age})
 
 if __name__=="__main":
