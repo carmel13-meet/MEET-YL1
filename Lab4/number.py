cb3a9d928cabad5dc5b0c547cce6e06c72211498
Carmel Paradis <carmel13@meet.mit.edu>
2014-02-20 19:30:06 +0200

    Lab4 -- DONE

--- Lab4/number.py
+++ Lab4/number.py
@@ -14,9 +14,19 @@ class NegativeInteger(Integer):
 		print "This is an object of the NegativeInteger class"
 		return self.num*(-1)
 if __name__ == "__main__":
-	test = NegativeInteger(9)
-        test2 = Integer(4, True)
-	myList = [test, test2]
-	for i in myList:
-		print i.display()
+	print "How many numbers do you want to create ? "
+	answer = int(raw_input())
+	for i in range(0, answer):
+		print "Give me a number "
+		yourNum = int(raw_input())
+		print "Is it positive or negative ? "
+		answer2 = raw_input()
+		if answer2 == "positive" or answer2 == "Positive":
+			x = Integer(yourNum, False)
+			print x.display()
+		elif answer2 == "negative" or answer2 == "Negative":
+			x = NegativeInteger(yourNum)
+			print x.display()
+		else:
+			print "-- WRONG ANSWER --"
 	
